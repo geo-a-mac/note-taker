0bd9406c273b332be1d1110ceea25985e33b6331
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-03-07 13:00:22 -0500

    push functionality added

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const shortUniqueId = require('short-unique-id');
+const fs = require('fs');
 
-// const uid = new ShortUniqueId();
+const uid = new shortUniqueId();
 
-const db = require('./db/db');
+const notes  = require('./db/db');
 const PORT = process.env.PORT || 3001;
 
 // Express middleware
@@ -13,9 +14,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// create and file a new note
+const createNewNote = (body) => {
+    let curNotes = notes;
+    //generate uniqe id for new note
+    const id = uid();
+    //create a new note
+    const newNote = {};
+    newNote.title = body.title;
+    newNote.text = body.text;
+    newNote.id = id;
+    curNotes.push(newNote);
+    console.log(curNotes);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'), 
+        JSON.stringify(curNotes, null, 2));
+    return(curNotes);
+};
+
 // GET /api/notes returns notes.json as json
 app.get('/api/notes', (req, res) =>{
-    let results = db;
+    let results = notes;
+    //console.log('get /api/notes called');
+    //console.log(results);
     res.json(results);
 });
 
@@ -29,11 +50,15 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.post('/api/notes', (req,res) => {
+    const newNote = createNewNote(req.body);
+    res.json(newNote);
+});
+
 // Default response for any other request (Not Found)
 app.use((req,res) => {
     res.status(404).end();
 });
-
 
 // Start teh server
 app.listen(PORT, () => {
